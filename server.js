0e19ffae25df6d4252bde7644520833bbe442550
neha123s <neha_solanki9167@ymail.com>
2017-03-16 07:42:47 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var articleOne={
+    title:  'Article One',
+    heading:    'Article One',
+    date: '5th January,2017',
+    content :   `<p>This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.</p>
+
+<p>This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.</p>
+
+<p>This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.This my First article.</p> `
+    
+    
+};
+function articles(data){
+    var title=data.title;
+    var header=data.header;
+    var date=data.date;
+    var content=data.content;
+    
+    
+    var htmlTemplate=`<html>
+<head>
+<title>${title}</title>
+<link href='/ui/style.css' rel="stylesheet"/>
+</head>
+<body>
+<div class="container">
+<div class="header">
+<a href="index.html">HOME</a>
+</div>
+<hr/>
+<div>
+    <h3>${header}</h3>
+    </div>
+    <div>
+  ${date}
+  
+  </div>
+<div class="content">
+${contetn}
+</div>
+</div>
+</body>
+</html>`
+
+return htmlTemplate;
+}
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -10,7 +57,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(articles(articleOne));
 });
 app.get('/article-two',function(req,res){
     res.send('This is article Two!');

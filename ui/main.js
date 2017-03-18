b91d6318d389dca04590c4f8fd70a64d72bb77b4
neha123s <neha_solanki9167@ymail.com>
2017-03-18 04:47:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 var img=document.getElementById('nn');
-var margin=0;
+var marginLeft=0;
 function moveRight(){
-    margin=margin+10;
-    img.style.margin=margin+'px'; 
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px'; 
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,100);

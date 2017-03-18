b849956fd137fe0495e9738abb5d40dac7d405e1
neha123s <neha_solanki9167@ymail.com>
2017-03-18 06:43:49 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,4 +9,21 @@ img.onclick=function(){
     var interval=setInterval(moveRight,100);
 }
 
-var 
+var button=document.getElementById("b1");
+button.onclick=function(){
+//Create Request Object
+var request=new XmlHttpRequest();
+ //capture value
+ request.onreadystatechange=function(){
+     if(request.readystate==XmlRequest.DONE){
+         if(request.status==200){
+             var counter=request.responseText;
+             var span=document.getElementById("count");
+             span.innerHtml=counter;
+         }
+     }
+ }
+ request.open('GET','http://neha123s.imad.hasura-app.io/',true);
+ request.send(null);
+    
+}

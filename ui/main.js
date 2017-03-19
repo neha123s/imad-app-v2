512ff18c1f5a1d690571b972e207a9b482a12ff9
neha123s <neha_solanki9167@ymail.com>
2017-03-18 21:44:25 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ img.onclick=function(){
     var interval=setInterval(moveRight,100);
 }
 
-var button=document.getElementById("b1");
+var button=document.getElementById('b1');
 button.onclick=function(){
 //Create Request Object
 var request=new XMLHttpRequest();
@@ -18,7 +18,7 @@ var request=new XMLHttpRequest();
      if(request.readyState===XMLHttpRequest.DONE){
          if(request.status===200){
              var counter=request.responseText;
-             var span=document.getElementById("count");
+             var span=document.getElementById('count');
              span.innerHTML=counter.toString();
          }
      }

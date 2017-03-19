a3a36b641311766869ce3ea849efab7130970d1f
neha123s <neha_solanki9167@ymail.com>
2017-03-18 21:42:54 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ var request=new XMLHttpRequest();
          if(request.status===200){
              var counter=request.responseText;
              var span=document.getElementById("count");
-             span.innerHtml=counter.toString();
+             span.innerHTML=counter.toString();
          }
      }
  };
